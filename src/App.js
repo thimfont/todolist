4b6ae692e8f9bff2db75eb39da73a1d17aaa7dd4
TMF <thiago.melo@outsmartdigital.com.br>
2021-05-26 21:33:40 -0300

    feat: create layout for registration form and table list.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,50 @@
-function App() {
+import React from 'react'
+
+const App = () => {
   return (
-    <h1>Hello</h1>
-  );
+    <div className="container">
+      <h1>Todo List App</h1>
+
+      <div className="flex-row">
+        <div className="flex-large">
+          <h3>Add task</h3>
+
+          <form>
+            <label>Title</label>
+            <input type="text" name="title" value="" />
+
+            <label>Description</label>
+            <input type="text" name="description" value="" />
+
+            <button>Add new task</button>
+          </form>
+        </div>
+
+        <div className="flex-large">
+          <h3>Table task list</h3>
+          <table>
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Description</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Study</td>
+                <td>I have study inglish</td>
+                <td>
+                  <button className="button muted-button">Edit</button>
+                  <button className="button muted-button">Delete</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App;
