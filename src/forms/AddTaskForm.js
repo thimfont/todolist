29f4c9cfc017d62160277c32b2e312912e1e4784
TMF <thiago.melo@outsmartdigital.com.br>
2021-05-26 22:05:32 -0300

    feat: add state to form.

--- src/forms/AddTaskForm.js
+++ src/forms/AddTaskForm.js
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AddTaskForm = () => {
+    const initialFormState = {
+        id: null,
+        title: '',
+        description: '',
+    }
+
+    const [task, setTask] = useState(initialFormState)
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target
+        setTask({ ...task, [name]: value })
+    }
+
     return (
         <form>
             <label>Title</label>
-            <input type="text" name="title" value="" />
+            <input
+                type="text"
+                name="title"
+                value={task.title}
+                onChange={handleInputChange} />
 
             <label>Description</label>
-            <input type="text" name="description" value="" />
+            <input
+                type="text"
+                name="description" value={task.description}
+                onChange={handleInputChange} />
 
             <button>Add new task</button>
         </form>
